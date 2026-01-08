7ec671f1f2ece5d30c0bd023609ccbfbe928f4d0
Nick Nisi <nick.nisi@workos.com>
2026-01-08 16:06:41 -0600

    fix build

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,7 +3,14 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  main: {},
+  main: {
+    build: {
+      // Exclude @workos-inc/node from externalization so it gets bundled
+      externalizeDeps: {
+        exclude: ['@workos-inc/node']
+      }
+    }
+  },
   preload: {},
   renderer: {
     resolve: {
